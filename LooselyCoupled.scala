1f3636cf5610dfdfe1de26258b2f675cab4b4de7
Sergey Scherbina <sergey.scherbina@gmail.com>
2013-06-30 19:38:34 +0300

    sbt-idea and sbt-eclipse are now happy with resulted build structure

--- LooselyCoupled.scala
+++ LooselyCoupled.scala
@@ -1,6 +1,10 @@
 import sbt._
 import sbt.Keys._
 import sbt.Load._
+import sbt.ResolvedClasspathDependency
+import scala.Some
+import scala.Some
+import scala.Some
 
 object LooselyCoupled extends Plugin {
 
@@ -21,10 +25,20 @@ object LooselyCoupled extends Plugin {
 
   def linkBuilds = buildsLinker(linkProject)
 
-  def buildsLinker(linker: ProjectLinker) =
-    addSettings(inProjects = Seq(buildDependencies in Global <<=
+  def buildsLinker(linker: ProjectLinker) = addSettings(
+    inProjects = Seq(buildDependencies in Global <<=
       (buildDependencies in Global, libraryDependencies,
-        thisProjectRef, organization)(linker)))
+        thisProjectRef, organization)(linker)),
+    inBuilds = Seq(onLoad in Global := onLoadBuild)
+  )
+
+  def onLoadBuild(state: State) = state get stateBuildStructure flatMap {
+    structure => (buildDependencies in Global) get structure.data map {
+      dependencies => state.copy(attributes = state.attributes.put(
+        stateBuildStructure, linkBuildStructure(structure, dependencies)))
+    }
+  } getOrElse state
+
 
   def addSettings(inBuilds: Seq[Setting[_]] = Seq(),
                   inProjects: Seq[Setting[_]] = Seq(),
@@ -55,6 +69,56 @@ object LooselyCoupled extends Plugin {
         ResolvedClasspathDependency(p, None)
     }
     builds.addClasspath(project, deps.toSeq: _*)
+  }
+
+  def linkBuildStructure(structure: BuildStructure,
+                         dependencies: BuildDependencies) = {
+
+    def unitFrom(u: BuildUnit) = {
+
+      def depends(p: Project) = {
+        dependencies.classpath.get(ProjectRef(u.uri, p.id)).map {
+          _.foldLeft(p)((p, d) => p.dependsOn(d.project))
+        }.getOrElse(p)
+      }
+
+      def buildFrom(b: Build) = new Build {
+
+        override def buildLoaders = b.buildLoaders
+
+        override def settings = b.settings
+
+        override def projects = b.projects map depends
+
+      }
+
+      def defsFrom(d: LoadedDefinitions) = new LoadedDefinitions(
+        d.base, d.target, d.loader, d.builds map buildFrom, d.buildNames)
+
+      new BuildUnit(u.uri, u.localBase, defsFrom(u.definitions), u.plugins)
+    }
+
+    def definedFrom(buildUri: URI)(d: (String, ResolvedProject)) = {
+      val (u, p) = d
+      def rdeps(ds: Seq[ClasspathDep[ProjectRef]]) = ds.map {
+        cp => ResolvedClasspathDependency(cp.project, cp.configuration)
+      }
+      def depends = dependencies.classpath.getOrElse(
+        ProjectRef(buildUri, p.id), Seq())
+      (u, Project.resolved(p.id, p.base, p.aggregate,
+        rdeps(p.dependencies) ++ rdeps(depends),
+        p.delegates, p.settings, p.configurations))
+    }
+
+    def mapUnit(u: (URI, LoadedBuildUnit)) = (u._1, new LoadedBuildUnit(
+      unitFrom(u._2.unit), u._2.defined map definedFrom(u._1),
+      u._2.rootProjects, u._2.buildSettings))
+
+    def structureFrom(s: BuildStructure) = new BuildStructure(
+      s.units map mapUnit, s.root, s.settings, s.data,
+      s.index, s.streams, s.delegates, s.scopeLocal)
+
+    structureFrom(structure)
   }
 
 }
